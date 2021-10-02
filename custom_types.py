49463b64b38b142e87dabfd7ff7bfa2812c30508
Adam Jacobsson <adam.jacobsson@users-air.home>
2021-10-02 21:20:43 +0200

    module

--- custom_types.py
+++
@@ -1,5 +0,0 @@
-class AttrDict(dict):
-    def __getattr__(self, attr):
-        item = self[attr]['callable']
-        instance_type = self[attr]['instance_type']
-        return item if instance_type == 'singleton' else item()
